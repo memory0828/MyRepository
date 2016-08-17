a34591e8fe602e77b0d1a7b80c39037322ebce03
memory0828 <memory0828@nate.com>
2016-08-17 14:54:41 +0900

    저장

---
+++ JavaProgramming/src/HellhWorld.java
@@ -0,0 +1,16 @@
+//클레스 선언부
+public class HellhWorld { //클래스클록
+	//메소드 선언부
+	public static void main(String[] args) { //메소드 블록
+		// TODO Auto-generated method stub
+		
+		//클래스이름의 첫자는 대문자
+		//두개이상의 단어가 모이면 그 단어의 시작은 대문자로 하는게 관행
+		//{} : 클래스 블록
+		//이 함수는 메인 메소드(함수가X다)
+		
+		System.out.println("A");	
+		System.out.println("B");
+	}
+
+}
