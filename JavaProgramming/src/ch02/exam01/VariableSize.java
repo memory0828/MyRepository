0b29f746bad4f4ed7644794f2eaad9444662568d
memory0828 <memory0828@nate.com>
2016-08-18 16:37:52 +0900

    exam02 소스추가

--- JavaProgramming/src/ch02/exam01/VariableSize.java
+++ JavaProgramming/src/ch02/exam01/VariableSize.java
@@ -4,19 +4,21 @@ public class VariableSize {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		byte var1 = 10;
+		//byte var1 = 10;
 		//byte var2 = 200;
 		
-		int var3 = 1000;
+		//int var3 = 1000;
 		//byte var4 = var3;
 		
-		int x = 200;
+		//int x = 200;
 		//byte b = (byte)x;
 		//System.out.println(b);
 		
-		int var5 = 200000;
+		//int var5 = 200000;
 		//char var6 = (char)var5;
-		//System.out.println(var6);		
+		//System.out.println(var6);
+		
+		
 	}
 
 }
